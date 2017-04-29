60eb0234c6c4bde04592d369730da578beb1555b
Feross Aboukhadijeh <feross@feross.org>
2017-04-28 18:00:57 -0700

    Use HTTPS clone URL (fix tests on travis)

--- test/basic.js
+++ test/basic.js
@@ -3,24 +3,25 @@ const path = require('path')
 const rimraf = require('rimraf')
 const test = require('tape')
 
-const TMP = path.join(__dirname, '..', 'tmp')
-const OUT_PATH = path.join(TMP, 'standard')
+const TMP_PATH = path.join(__dirname, '..', 'tmp')
+const OUT_PATH = path.join(TMP_PATH, 'standard')
+const REPO_URL = 'https://github.com/feross/git-pull-or-clone.git'
 
 test('remove tmp folder', (t) => {
-  rimraf.sync(TMP)
+  rimraf.sync(TMP_PATH)
   t.end()
 })
 
 test('git clone', (t) => {
   t.plan(1)
-  gitPullOrClone('git@github.com:feross/standard.git', OUT_PATH, (err) => {
+  gitPullOrClone(REPO_URL, OUT_PATH, (err) => {
     t.error(err)
   })
 })
 
 test('git pull', (t) => {
   t.plan(1)
-  gitPullOrClone('git@github.com:feross/standard.git', OUT_PATH, (err) => {
+  gitPullOrClone(REPO_URL, OUT_PATH, (err) => {
     t.error(err)
   })
 })
